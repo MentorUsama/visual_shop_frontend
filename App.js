c9ef0903eaf7cf3a031e0a8e4dbdbcedea79d6f4
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2021-12-24 19:43:04 +0500

    FontLoaded

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// Importing Fonts
+import * as Font from 'expo-font';
+// Importing App Loading
+import AppLoading from 'expo-app-loading';
 // Redux Imports
 import { Provider } from 'react-redux';
-import { createStore, compose, combineReducers } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import userReducer from './store/Reducers/userReducer'
 // Importing Screens
-import Home from './pages/Home/Home'
+import Home from './pages/Home/Home';
 
 
 
+// Importing Fonts
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'Roboto-Black': require('./assets/fonts/Roboto-Black.ttf'),
+    'Roboto-Light': require('./assets/fonts/Roboto-Light.ttf'),
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+  });
+};
 
-// Settingup Stores
+// Setting up Stores
 const rootReducer = combineReducers({ userReducer: userReducer });
 const store = createStore(rootReducer);
 
 export default function App() {
+  // Makin Sure Fonts Are Loaded
+  const [fontLoaded, setFontLoaded] = useState(false);
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
+  // Returning The Screen If loaded
   return (
     <Provider store={store}>
       <View style={styles.container}>
@@ -23,7 +49,7 @@ export default function App() {
     </Provider>
   );
 }
-
+// Styling
 const styles = StyleSheet.create({
   container: {
     flex: 1,
