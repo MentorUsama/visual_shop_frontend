c6ce0f50cac6c25a5d02b4120843436996b82c2d
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2021-12-31 11:52:58 +0500

    module.css functionality added

--- components/container/PageContainer.js
+++ components/container/PageContainer.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { View, Text,Button,SafeAreaView  } from 'react-native'
+import { useTheme } from 'react-native-paper';
+import styles from './PageContainer.module.css';
 
 export default function PageContainer(props) {
+    const { colors } = useTheme();
     return (
-        <SafeAreaView style={{backgroundColor:"#EFEFEF",flex:1}}>
-            <View style={{paddingLeft:36,paddingRight:36}}>
-                <View style={{marginTop:50,marginBottom:50}} >
+        <SafeAreaView style={[styles.saveArea,{backgroundColor:colors.background}]}>
+            <View style={styles.mainContainer}>
+                {/* Navigations */}
+                <View>
                     <Text>This is Navigtion Start </Text>
                     <Button title='OpenDrawer' onPress={()=>props.navigation.toggleDrawer()}/>
                     {props.navigation.canGoBack()?<Button title='Back' onPress={()=>props.navigation.goBack()} />:null}
                     <Text>This is Navigtion End </Text>
                 </View>
+                {/* All Childerens */}
                 <View>
                     {props.children}
                 </View>
