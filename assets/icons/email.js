e585ec4370ce922ca90bb1e20ba0712e07eb7865
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2022-01-19 19:18:37 +0500

    About Page Added

--- assets/icons/email.js
+++ assets/icons/email.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import Svg, { Path } from "react-native-svg"
 
 function SvgComponent(props) {
-  const {width=23.588,height=18.155}=props;
+  const {width=23.588,height=18.155,fill="#040606"}=props;
   return (
     <Svg
       data-name="Group 139"
@@ -15,12 +15,12 @@ function SvgComponent(props) {
       <Path
         d="M688.975-261.565l8.2 9.3a2.454 2.454 0 001.842.832 2.456 2.456 0 001.841-.832l8.054-9.138a.855.855 0 01.229-.181 2.762 2.762 0 00-1.114-.234H690a2.773 2.773 0 00-1.069.214c.013.013.029.025.042.039"
         transform="translate(-687.218 261.818)"
-        fill={props.fill}
+        fill={fill}
       />
       <Path
         d="M708.664-259.207a.855.855 0 01-.158.265l-8.054 9.138a4.205 4.205 0 01-3.153 1.425 4.2 4.2 0 01-3.154-1.425l-8.2-9.3a.7.7 0 01-.043-.06 2.766 2.766 0 00-.4 1.435v12.587a2.784 2.784 0 002.784 2.784h18.021a2.784 2.784 0 002.783-2.784v-12.587a2.764 2.764 0 00-.428-1.479"
         transform="translate(-685.504 260.513)"
-        fill={props.fill}
+        fill={fill}
       />
     </Svg>
   )
--- pages/About/About.js
+++ pages/About/About.js
@@ -1,13 +1,76 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
 import PageContainer from '../../components/container/PageContainer'
+import Phone from '../../assets/icons/phone'
+import Email from '../../assets/icons/email'
 
 
-const About=(props)=>{
+const About = (props) => {
     return (
-        <PageContainer navigation={props.navigation}>
-            <Text>This is About Page</Text>
+        <PageContainer hasPadding={true} navigation={props.navigation}>
+            <ScrollView>
+                <View style={styles.containerBottomGap}>
+                    <Text style={styles.title}>About Company</Text>
+                    <Text style={styles.textStyle}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing.</Text>
+                </View>
+                <View>
+                    <Text style={[styles.title, styles.containerBottomGap]}>Privacy And Policy</Text>
+                    <View style={[styles.subcontainerLeftGap, styles.containerBottomGap]}>
+                        <Text style={styles.subTitle}>1) Privacy And Policy</Text>
+                        <Text style={styles.textStyle}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of </Text>
+                    </View>
+                    <View style={[styles.subcontainerLeftGap, styles.containerBottomGap]}>
+                        <Text style={styles.subTitle}>2) Returned Policy</Text>
+                        <Text style={styles.textStyle}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of </Text>
+                    </View>
+                </View>
+                <View style={styles.containerBottomGap}>
+                    <Text style={[styles.title,styles.containerBottomGap]}>Contact Us</Text>
+                    <View style={styles.subcontainerLeftGap}>
+                        <View style={styles.iconContainer}>
+                            <View style={styles.icon}><Phone /></View>
+                            <Text style={styles.textStyle}>03772947540</Text>
+                        </View>
+                        <View style={styles.iconContainer}>
+                            <View style={styles.icon}><Email /></View>
+                            <Text style={styles.textStyle}>Usama.farhat.08@gmail.com</Text>
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
         </PageContainer>
     )
 }
 export default About;
+const styles = StyleSheet.create({
+    containerBottomGap: {
+        marginBottom: 20
+    },
+    subcontainerLeftGap: {
+        paddingLeft: 30
+    },
+    title: {
+        color: '#000000',
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginBottom: 5
+    },
+    subTitle: {
+        color: '#000000',
+        fontSize: 15,
+        fontWeight: 'bold',
+        marginBottom: 5
+    },
+    textStyle: {
+        color: '#000000',
+        fontSize: 12,
+    },
+    iconContainer:{
+        display:'flex',
+        flexDirection:'row',
+        marginBottom:20
+    },
+    icon:{
+        width:40
+    }
+})
