b316a75703b40770bb8ea9be34f4fe702366900d
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2022-01-30 20:57:27 +0500

    orders page added

--- pages/Orders/Orders.js
+++ pages/Orders/Orders.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import { Text, View,Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, Button, ScrollView } from 'react-native';
 import PageContainer from '../../components/container/PageContainer'
+import { getOrders } from '../../Utility/APIS/Order/order'
+import OrderDetail from '../../components/components/Orders/orderDetail';
+import { getSelectedImage } from '../../Utility/HelperFunctions/index'
+// Redux
+import { connect } from 'react-redux';
+import * as actions from '../../store/Actions/index'
 
-const Orders=(props)=>{
+const Orders = (props) => {
+    const [pageLoading, setPageLoading] = useState(false);
+    useEffect(async () => {
+        if (props.orders == null) {
+            setPageLoading(true)
+            const response = await getOrders(props.access)
+            if (response.status == 200) {
+                props.addOrders(response.data)
+            }
+            setPageLoading(false)
+        }
+    }, [])
     return (
-        <PageContainer navigation={props.navigation}>
-            <Text>This is Orders Page</Text>
-            <View style={{marginTop:10}}></View>
-            <Button onPress={() => props.navigation.navigate('ComplaintsDetail')} title="Add/See Complaints Of this Order"/>
+        <PageContainer hasPadding={true} pageLoading={pageLoading} navigation={props.navigation}>
+            <View>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    {
+                        props.orders && props.orders.map(order => {
+                            return (
+                                <OrderDetail getSelectedImage={getSelectedImage} key={order.id} order={order} />
+                            )
+                        })
+                    }
+                </ScrollView>
+            </View>
         </PageContainer>
     )
 }
-export default Orders;
+const mapStateToProps = state => {
+    return {
+        orders: state.orderReducer.orders,
+        access: state.userReducer.access,
+    };
+};
+const mapDispatchToProps = dispatch => {
+    return {
+        addOrders: (orders) => dispatch(actions.addOrders(orders))
+    };
+};
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Orders);
